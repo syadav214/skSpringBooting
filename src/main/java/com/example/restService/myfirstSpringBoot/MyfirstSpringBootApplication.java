41bca34740d857266e34a1dce0f02acdd6ea7159
Santoshkumar Yadav <syadav@cimpress.com>
2020-02-11 13:40:05 +0530

    initial commit

---
+++ src/main/java/com/example/restService/myfirstSpringBoot/MyfirstSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.restService.myfirstSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restService/myfirstSpringBoot/MyfirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restService.myfirstSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
