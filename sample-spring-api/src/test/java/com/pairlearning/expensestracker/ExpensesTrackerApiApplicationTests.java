575a5db8dff329e10a9df44860167e3dc6437cd8
Santoshkumar Yadav <syadav214@gmail.com>
2020-08-01 19:30:15 +0530

    added sample spring

---
+++ sample-spring-api/src/test/java/com/pairlearning/expensestracker/ExpensesTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pairlearning.expensestracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
