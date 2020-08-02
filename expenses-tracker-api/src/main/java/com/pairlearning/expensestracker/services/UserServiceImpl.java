2319b9f4cb4d2827cc61b2164f6e02d64f268d6e
Santoshkumar Yadav <syadav214@gmail.com>
2020-08-02 13:59:10 +0530

    Added insert statement

--- expenses-tracker-api/src/main/java/com/pairlearning/expensestracker/services/UserServiceImpl.java
+++ expenses-tracker-api/src/main/java/com/pairlearning/expensestracker/services/UserServiceImpl.java
@@ -2,8 +2,19 @@ package com.pairlearning.expensestracker.services;
 
 import com.pairlearning.expensestracker.domain.User;
 import com.pairlearning.expensestracker.exceptions.EtAuthException;
+import com.pairlearning.expensestracker.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.regex.Pattern;
+
+@Service
+//@Transactional
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public User validateUser(String email, String password) throws EtAuthException {
@@ -12,6 +23,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User registerUser(String firstName, String lastName, String email, String password) throws EtAuthException {
-        return null;
+        Pattern pattern = Pattern.compile("^(.+)@(.+)$");
+        if(email !=null) email = email.toLowerCase();
+        if(!pattern.matcher(email).matches())
+            throw new EtAuthException("Invalid email format");
+        Integer count = userRepository.getCountByEmail(email);
+        if(count > 0)
+            throw new EtAuthException("Email already in use");
+        Integer userId = userRepository.create(firstName, lastName, email, password);
+        return userRepository.findById(userId);
     }
 }
