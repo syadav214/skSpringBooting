64cd33fe6cbdb2689169311aa1bbb07454a28037
Santoshkumar Yadav <syadav214@gmail.com>
2020-08-05 15:33:47 +0530

    used filters

--- expenses-tracker-api/src/main/java/com/pairlearning/expensestracker/ExpensesTrackerApiApplication.java
+++ expenses-tracker-api/src/main/java/com/pairlearning/expensestracker/ExpensesTrackerApiApplication.java
@@ -1,13 +1,29 @@
 package com.pairlearning.expensestracker;
 
+import com.pairlearning.expensestracker.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.FilterRegistration;
 
 @SpringBootApplication
 public class ExpensesTrackerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpensesTrackerApiApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterFilterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		// login and register can be access by any one
+		// other endpoint need to authenticated such as categories
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return  registrationBean;
 	}
 
 }
